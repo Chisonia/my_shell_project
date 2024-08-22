b12e714b7dc0b8886ab50e51b9b78ab7669f8e47
chisonia <sonya12slyvia@gmail.com>
2024-08-22 06:01:30 +0100

    Update

--- shell.h
+++ shell.h
@@ -1,20 +1,9 @@
-#ifndef SHELL_H
-#define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#define MAX_TOKENS 64
-#define MAX_PATH_LENGTH 1024
-
-void display_prompt(void);
-char *read_command();
-void execute_command(char **tokens);
-void free_tokens(char **tokens);
-char **tokenize(char *cmd);
-char *find_char(const char *str, char target);
-#endif
+void execmd(char **argv);
+char *find_location(char *command);
